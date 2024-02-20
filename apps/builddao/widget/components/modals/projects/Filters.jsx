8a4ad8d17c7447d48a6c0aa49fe05c1a878650b0
Zeeshan Ahmad <itexpert120@outlook.com>
2024-02-20 23:28:19 +0500

    Add Projects Feed

---
+++ apps/builddao/widget/components/modals/projects/Filters.jsx
@@ -0,0 +1,68 @@
+const { Button } = VM.require("buildhub.near/widget/components") || {
+  Button: () => <></>,
+};
+
+const [teamSize, setTeamSize] = useState("");
+const [tags, setTags] = useState([]);
+
+const handleTags = (tags) => {
+  let filtered = tags.map((tag) => (tag.customOption ? tag.label : tag));
+  setTags(filtered);
+};
+
+return (
+  <div className="d-flex flex-column gap-3">
+    <div className="d-flex flex-column gap-3">
+      <label>Popular Tags</label>
+      <div className="d-flex align-items-center gap-2 flex-wrap">
+        <Button variant="outline">Community</Button>
+        <Button variant="outline">Filter</Button>
+        <Button variant="outline">Filter</Button>
+        <Button variant="outline">Filter</Button>
+      </div>
+    </div>
+    <div className="d-flex flex-column gap-3" key={"team-sizes"}>
+      <label>Team Size</label>
+      <div className="d-flex align-items-center gap-2 flex-wrap">
+        <Button
+          key={"1-10"}
+          variant="outline"
+          onClick={() => setTeamSize("1-10")}
+        >
+          1-10
+        </Button>
+        <Button
+          key={"10-50"}
+          variant="outline"
+          onClick={() => setTeamSize("10-50")}
+        >
+          10-50
+        </Button>
+        <Button
+          key={"50-100"}
+          variant="outline"
+          onClick={() => setTeamSize("50-100")}
+        >
+          50-100
+        </Button>
+        <Button
+          key={"100+"}
+          variant="outline"
+          onClick={() => setTeamSize("100+")}
+        >
+          100+
+        </Button>
+      </div>
+    </div>
+    <div className="form-group">
+      <label className="mb-3">Tag</label>
+      <Typeahead
+        multiple
+        options={[]}
+        allowNew
+        selected={tags}
+        onChange={(e) => handleTags(e)}
+      />
+    </div>
+  </div>
+);
---
+++ apps/builddao/widget/page/project-feed.jsx
@@ -0,0 +1,145 @@
+const { fetchThings } = VM.require(
+  "buildhub.near/widget/lib.everything-sdk"
+) || {
+  fetchThings: () => {},
+};
+
+const { Button } = VM.require("buildhub.near/widget/components") || {
+  Button: () => <></>,
+};
+
+const { ProjectCard } = VM.require(
+  "buildhub.near/widget/components.project.Card"
+) || {
+  ProjectCard: () => <></>,
+};
+
+// const projects = fetchThings("buildbox", "project");
+
+const projects = [
+  {
+    title: "Build DAO",
+    accountId: "build.sputnik-dao.near",
+    tags: ["Open Source", "Community"],
+    collaborators: ["efiz.near"],
+  },
+  {
+    title: "Build DAO 1",
+    accountId: "build.sputnik-dao.near",
+    tags: ["Open Source", "Community"],
+    collaborators: ["efiz.near", "dawnkelly.near"],
+  },
+  {
+    title: "Build DAO 2",
+    accountId: "build.sputnik-dao.near",
+    tags: ["Open Source", "Community"],
+    collaborators: ["efiz.near", "dawnkelly.near", "james.near"],
+  },
+  {
+    title: "Build DAO 3",
+    accountId: "build.sputnik-dao.near",
+    tags: ["Open Source", "Community"],
+    collaborators: [
+      "efiz.near",
+      "dawnkelly.near",
+      "james.near",
+      "itexpert120-contra.near",
+    ],
+  },
+];
+
+if (!projects) {
+  return "";
+}
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, minmax(0, 1fr));
+  gap: 1rem;
+
+  @media screen and (max-width: 960px) {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+  }
+
+  @media screen and (max-width: 768px) {
+    grid-template-columns: repeat(1, minmax(0, 1fr));
+  }
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+`;
+
+const [filters, setFilters] = useState({
+  title: "",
+});
+const [showModal, setShowModal] = useState(false);
+
+const toggleModal = () => {
+  setShowModal((prev) => !prev);
+};
+
+const filteredProjects = useMemo(() => {
+  let filtered = projects;
+  if (filters.title !== "") {
+    filtered = filtered.filter((project) =>
+      project.title.toLowerCase().includes(filters.title.toLowerCase())
+    );
+  }
+  return filtered;
+}, [filters, projects]);
+
+return (
+  <Wrapper
+    className="container-xl mx-auto"
+    style={{ margin: "24px 0" }}
+    data-bs-theme="dark"
+  >
+    <Widget
+      src="buildhub.near/widget/components.modals.FilterProjects"
+      props={{
+        showModal: showModal,
+        toggleModal: toggleModal,
+      }}
+    />
+    <div className="my-3 d-flex align-items-center justify-content-between">
+      <h2 style={{ color: "var(--text-color, #fff)", fontSize: "18px" }}>
+        Projects
+      </h2>
+      <Button variant="primary">Create Project</Button>
+    </div>
+    <div className="form-group d-flex gap-4 align-items-center justify-content-between">
+      <div className="input-group">
+        <div className="input-group-text">
+          <i className="bi bi-search"></i>
+        </div>
+        <input
+          className="form-control"
+          style={{ borderLeft: "none" }}
+          placeholder="Search by project ID or name"
+          value={filters.title}
+          onChange={(e) => setFilters({ ...filters, title: e.target.value })}
+        />
+      </div>
+      <Button
+        className="d-flex align-items-center"
+        style={{ gap: 10, padding: "10px 26px" }}
+        onClick={() => setShowModal(true)}
+      >
+        Filter <i className="bi bi-sliders"></i>
+      </Button>
+    </div>
+    <Container>
+      {filteredProjects.map((project) => (
+        <ProjectCard
+          title={project.title}
+          accountId={project.accountId}
+          tags={project.tags}
+          people={project.collaborators}
+        />
+      ))}
+    </Container>
+  </Wrapper>
+);
