fe1548a76c574cee8140aebcd1a6e7f51db55976
jaswinder6991 <jassmultani2@gmail.com>
2024-02-22 16:24:30 +0530

    updated drop id and trial url

--- src/components/TrialAccountGenerator.js
+++ src/components/TrialAccountGenerator.js
@@ -2,8 +2,8 @@ export const TrialAccountGenerator = ({ trigger }) => {
   async function getTrialAccount() {
     try {
       const response = await fetch(
-        `https://harmonicdevapim.azure-api.net/bd/KeyPomMain?dropId=1706695349746`,
-        { method: "POST" }
+        `https://harmonicdevapim.azure-api.net/bd/KeyPomMain?dropId=1708596356159`,
+        { method: "POST" },
       );
       if (!response.ok) {
         // Handle HTTP errors
@@ -11,16 +11,18 @@ export const TrialAccountGenerator = ({ trigger }) => {
       }
       const body = await response.json(); // Correctly parse the JSON response body
 
-      //To-do
-      // Do I really need to get the trial account path like this?
-      // It does help me make localhost work for local testing. Need to change to trial URL in app.js to make it work though.
-      const path = body.url.split("https://www.nearbuilders.org")[1];
-
+      // //To-do
+      // // Do I really need to get the trial account path like this?
+      // // It does help me make localhost work for local testing. Need to change to trial URL in app.js to make it work though.
+      const path = "/join?from=trial" +body.url.split("https://www.nearbuilders.org")[1];
+  
       //This does not work right now because of keypom selector is implemented.
       //window.location.href = `${window.location.origin}${path}`;
 
+      console.log(`${window.location.origin}${path}`)
       window.open(`${window.location.origin}${path}`, "_self");
-      window.location.reload();
+      //window.location.reload();
+      
     } catch (error) {
       console.error("Failed to get trial account:", error);
       throw new Error(error);
