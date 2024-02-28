8328c57e4d1e296ec9e75ecafd4cdff721058461
Zeeshan Ahmad <itexpert120@outlook.com>
2024-02-28 21:32:51 +0500

    Formatting fix

--- apps/builddao/widget/page/home.jsx
+++ apps/builddao/widget/page/home.jsx
@@ -1,5 +1,5 @@
 const { Hero, Goals, Join, Purposes, AboutUs, Governance, Footer } = VM.require(
-  "buildhub.near/widget/home.Home"
+  "buildhub.near/widget/home.Home",
 ) || {
   Hero: () => <></>,
   Goals: () => <></>,
