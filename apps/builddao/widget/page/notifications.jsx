e008d4d1ee55bc3884af33818a06376aa29baff7
Zeeshan Ahmad <itexpert120@outlook.com>
2024-05-13 13:43:25 -0700

    Prompt for login on notifications page

--- apps/builddao/widget/page/notifications.jsx
+++ apps/builddao/widget/page/notifications.jsx
@@ -1,7 +1,35 @@
+const { Button } = VM.require("buildhub.near/widget/components") || {
+  Button: () => <></>,
+};
+
 const accountId = context.accountId;
 
+const LoginContainer = styled.div`
+  background-color: #23242b;
+  color: #fff;
+
+  width: 100%;
+  height: 16rem;
+  border-radius: 1rem;
+
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  align-items: center;
+  justify-content: center;
+
+  margin-bottom: 1rem;
+`;
+
 if (!accountId) {
-  return "Sign in with NEAR Wallet";
+  return (
+    <LoginContainer>
+      <p>Please login in order to see notifications.</p>
+      <a href={"${alias_gateway_url}/join"} style={{ textDecoration: "none" }}>
+        <Button variant="primary">Login</Button>
+      </a>
+    </LoginContainer>
+  );
 }
 
 const index = {
