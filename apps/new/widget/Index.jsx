6ad413b4e0cd492c188577c106c454852f1df62e
Zeeshan Ahmad <itexpert120@outlook.com>
2024-05-21 01:28:57 +0500

    Initialize with essential routes

--- apps/new/widget/Index.jsx
+++ apps/new/widget/Index.jsx
@@ -1,1 +1,30 @@
-return <p>hello ${config_account}</p>;
+const config = {
+  theme: {},
+  layout: {
+    src: "${alias_devs}/widget/Layout",
+    props: {
+      variant: "standard",
+    },
+  },
+  blocks: {
+    Header: () => <></>,
+    Footer: () => <></>,
+  },
+  router: {
+    param: "page",
+    routes: {
+      home: {
+        path: "${config_account}/widget/home.Home",
+        blockHeight: "final",
+        init: {
+          name: "Home",
+        },
+        default: true,
+      },
+    },
+  },
+};
+
+return (
+  <Widget src="buildhub.near/widget/app.view" props={{ config, ...props }} />
+);
