84c7ae6f0da80b10a665a02e52efcf4e420d4fca
Megha-Dev-19 <100185149+Megha-Dev-19@users.noreply.github.com>
2024-06-03 21:53:59 +0530

    update login container css

--- apps/new/widget/components/LoginAction.jsx
+++ apps/new/widget/components/LoginAction.jsx
@@ -2,30 +2,40 @@ const { Button } = VM.require("${alias_old}/widget/components") || {
   Button: () => <></>,
 };
 
+const Logo =
+  "https://ipfs.near.social/ipfs/bafkreihsuyli6i2wphsutag6xcxnyhyrn7wtkklvqebx4szgpz3ieqacxu";
+
 const LoginContainer = styled.div`
-  background-color: #23242b;
   color: #fff;
 
   width: 100%;
-  height: 16rem;
+  height: 100%;
   border-radius: 1rem;
 
   display: flex;
   flex-direction: column;
-  gap: 0.5rem;
+  gap: 0.8rem;
   align-items: center;
   justify-content: center;
 
   margin-bottom: 1rem;
 `;
 
+const LogoStyle = styled.img`
+  width: 121.949px;
+  height: 87.799px;
+  transform: rotate(-4.223deg);
+`;
+
 const text = props.text;
 
 return (
   <LoginContainer>
-    {text && <p>{text}</p>}
+    <LogoStyle src={Logo} />
+    <h4>You’re not connected</h4>
+    {text && <h6>{text}</h6>}
     <a href={"${alias_gateway_url}/join"} style={{ textDecoration: "none" }}>
-      <Button variant="primary">Login</Button>
+      <Button variant="primary">Connect</Button>
     </a>
   </LoginContainer>
 );
