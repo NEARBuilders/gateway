5af57474471d51de70bc6754dd3f9fa5f05ad789
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2024-06-16 10:30:58 -0400

    Defaults fallback for Object.keys (fix red flash on projects page) (#396)
    * increment
    
    * adds fallbacks
    
    * fmt

--- apps/new/widget/lib/projects.jsx
+++ apps/new/widget/lib/projects.jsx
@@ -4,8 +4,8 @@ const getTagsInArray = (tags) => {
 
 const flattenObject = (obj, app, type) => {
   const paths = [];
-  for (const key of Object.keys(obj)) {
-    const projects = Object.keys(obj?.[key]?.[app]?.[type] ?? {});
+  for (const key of Object.keys(obj || {})) {
+    const projects = Object.keys(obj?.[key]?.[app]?.[type] || {});
     for (const project of projects) {
       if (project && project.includes("_")) {
         const convertedStr = project.replace(/_/g, "/");
@@ -57,7 +57,7 @@ const processData = (data, type) => {
           type,
           title: metadata.title,
           metadata,
-          tags: getTagsInArray(metadata.tags) || [],
+          tags: getTagsInArray(metadata.tags || []),
           collaborators: metadata.contributors,
           projectID,
         };
