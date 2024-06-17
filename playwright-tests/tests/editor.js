3a83b005850c6df999b0814da6959085cd9edc64
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2024-06-17 14:18:20 -0400

    init tests

---
+++ playwright-tests/tests/editor.js
@@ -0,0 +1,52 @@
+import { test, expect } from "@playwright/test";
+import { ROOT_SRC } from "../util/constants";
+
+test.describe("?page=projects&tab=editor", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(`/${ROOT_SRC}?page=projects`);
+  });
+
+  test.describe("User is not logged in", () => {
+    test.use({
+      storageState: "playwright-tests/storage-states/wallet-not-connected.json",
+    });
+
+    test("editor page should prompt user to log in", async ({ page }) => {});
+  });
+
+  test.describe("User is logged in", () => {
+    test.use({
+      storageState: "playwright-tests/storage-states/wallet-connected.json",
+    });
+
+    test("should not allow next when empty required fields", async ({
+      page,
+    }) => {
+      // title
+      // description
+    });
+
+    test("should complete flow and save data correctly with images populated", async ({
+      page,
+    }) => {
+      // title
+      // description
+      // tags
+      // images
+
+      // click button that triggers transaction
+      // await page.getByRole("button", { name: "Donate" }).nth(1).click();
+
+      // const transactionObj = JSON.parse(
+      //   await page.locator("div.modal-body code").innerText(),
+      // );
+
+      // // do something with transactionObj
+      // expect(transactionObj).toMatchObject({
+      //   amount: 100,
+      //   message: "",
+      //   projectId: DEFAULT_PROJECT_ID,
+      // });
+    });
+  });
+});
--- playwright-tests/tests/projects.spec.js
+++ playwright-tests/tests/projects.spec.js
@@ -1,40 +1,76 @@
-import { test, expect } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 import { ROOT_SRC } from "../util/constants";
 
-test.beforeEach(async ({ page }) => {
-  await page.goto(`/${ROOT_SRC}?page=projects`);
-});
+test.describe("?page=projects", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(`/${ROOT_SRC}?page=projects`);
+  });
 
-// (this may be changed to open preview modal rather than page)
-test("should navigate to project page on grid card click", async ({ page }) => {
-  // select grid layout
-  await page.getByTestId("grid-layout-button").click();
+  test("should navigate to project page on grid card click", async ({
+    page,
+  }) => {
+    // select grid layout
+    await page.getByTestId("grid-layout-button").click();
 
-  // click grid card
-  const projectGridCard = await page.getByTestId("project-grid-card").nth(1);
+    // click grid card
+    const projectGridCard = await page.getByTestId("project-grid-card").nth(1);
 
-  await expect(projectGridCard).toBeVisible({ timeout: 10000 });
+    await expect(projectGridCard).toBeVisible({ timeout: 10000 });
 
-  await Promise.all([projectGridCard.click(), page.waitForLoadState("load")]);
-  const projectViewBtn = page.getByRole("button", { name: "See Project page" });
-  await expect(projectViewBtn).toBeVisible();
-  await projectViewBtn.click();
-  expect(page.url()).toContain("?page=project&id=");
-});
+    await Promise.all([projectGridCard.click(), page.waitForLoadState("load")]);
+    const projectViewBtn = page.getByRole("button", {
+      name: "See Project page",
+    });
+    await expect(projectViewBtn).toBeVisible();
+    await projectViewBtn.click();
+    expect(page.url()).toContain("?page=project&id=");
+  });
 
-// (this may be changed to open preview modal rather than page)
-test("should navigate to project page on list card click", async ({ page }) => {
-  // select list layout
-  await page.getByTestId("list-layout-button").click();
+  test("should navigate to project page on list card click", async ({
+    page,
+  }) => {
+    // select list layout
+    await page.getByTestId("list-layout-button").click();
 
-  // click list card
-  const projectListCard = await page.getByTestId("project-list-card").nth(1);
+    // click list card
+    const projectListCard = await page.getByTestId("project-list-card").nth(1);
 
-  await expect(projectListCard).toBeVisible({ timeout: 10000 });
+    await expect(projectListCard).toBeVisible({ timeout: 10000 });
 
-  await Promise.all([projectListCard.click(), page.waitForLoadState("load")]);
-  const projectViewBtn = page.getByRole("button", { name: "See Project page" });
-  await expect(projectViewBtn).toBeVisible();
-  await projectViewBtn.click();
-  expect(page.url()).toContain("?page=project&id=");
+    await Promise.all([projectListCard.click(), page.waitForLoadState("load")]);
+    const projectViewBtn = page.getByRole("button", {
+      name: "See Project page",
+    });
+    await expect(projectViewBtn).toBeVisible();
+    await projectViewBtn.click();
+    expect(page.url()).toContain("?page=project&id=");
+  });
+
+  test.describe("User is not logged in", () => {
+    test.use({
+      storageState: "playwright-tests/storage-states/wallet-not-connected.json",
+    });
+
+    test("'create project' button should be hidden on projects page", async ({
+      page,
+    }) => {});
+  });
+
+  test.describe("User is logged in", () => {
+    test.use({
+      storageState: "playwright-tests/storage-states/wallet-connected.json",
+    });
+
+    test("'create project' button should be visable and bring up modal", async ({
+      page,
+    }) => {});
+
+    test("'create own project' should navigate to editor", async ({
+      page,
+    }) => {});
+
+    test("'import from potlock' should navigate to potlockImport", async ({
+      page,
+    }) => {});
+  });
 });
