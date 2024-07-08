33e3ddd0cf8a1b73a60fe83d6bbac4a7923649d0
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2024-07-08 12:29:34 -0400

    Adds web4 contract (#457)
    * init
    
    * tests pass, init
    
    * tests pass
    
    * configures workflow
    
    * update web component
    
    * sets default workflow wd
    
    * upgrade versions
    
    * add network
    
    * adds metadata
    
    * fmt
    
    * updates contract for testnet social db
    
    * set title

---
+++ web4contract/tests/test_basics.rs
@@ -0,0 +1,11 @@
+#[tokio::test]
+async fn test_contract_is_operational() -> Result<(), Box<dyn std::error::Error>> {
+    // let sandbox = near_workspaces::sandbox().await?;
+    // let contract_wasm = near_workspaces::compile_project("./").await?;
+
+    // let contract = sandbox.dev_deploy(&contract_wasm).await?;
+
+    // let user_account = sandbox.dev_create_account().await?;
+
+    Ok(())
+}
