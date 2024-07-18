5143919b81a1e81aa6a51fb1e60696fcc55f4e2a
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2024-07-17 20:01:12 -0400

    use every.near/widget/app.view

--- apps/new/widget/Index.jsx
+++ apps/new/widget/Index.jsx
@@ -137,7 +137,7 @@ const config = {
 return (
   <Container>
     <CSS>
-      <Widget src="${alias_old}/widget/app.view" props={{ config, ...props }} />
+      <Widget src="${alias_every}/widget/app.view" props={{ config, ...props }} />
     </CSS>
   </Container>
 );
