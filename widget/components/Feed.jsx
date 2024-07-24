4ee38b8b0f93be1d9d1accd9370fac46393ce2a1
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2024-07-24 16:28:19 -0600

    standardize to login container

--- widget/components/Feed.jsx
+++ widget/components/Feed.jsx
@@ -11,23 +11,6 @@ const { Header } = VM.require("${config_account}/widget/components.Header") || {
   Header: () => <></>,
 };
 
-const LoginContainer = styled.div`
-  background-color: #23242b;
-  color: #fff;
-
-  width: 100%;
-  height: 16rem;
-  border-radius: 1rem;
-
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  align-items: center;
-  justify-content: center;
-
-  margin-bottom: 1rem;
-`;
-
 const { feedName, template, requiredHashtags, customActions } = props;
 
 // for modals
@@ -67,15 +50,13 @@ return (
       }}
     />
     {!context.accountId ? ( // if not logged in
-      <LoginContainer>
-        <p>Please login in order to post.</p>
-        <a
-          href={"${alias_gateway_url}/join"}
-          style={{ textDecoration: "none" }}
-        >
-          <Button variant="primary">Login</Button>
-        </a>
-      </LoginContainer>
+      <Widget
+        src="${config_account}/widget/components.LoginAction"
+        loading=""
+        props={{
+          text: "Please login in order to post.",
+        }}
+      />
     ) : (
       <Widget
         loading={
