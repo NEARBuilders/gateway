4217f4b2d2b4dd6aaae106689680efda64d6e00e
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2024-07-24 13:32:12 -0600

    removes old gatewy

--- src/hooks/useQuery.js
+++
@@ -1,7 +0,0 @@
-import { useLocation } from "react-router-dom";
-import React from "react";
-
-export function useQuery() {
-  let { search } = useLocation();
-  return React.useMemo(() => new URLSearchParams(search), [search]);
-}
